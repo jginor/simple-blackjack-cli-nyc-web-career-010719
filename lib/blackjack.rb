5bed0bfa9538207580bca6d4cb8f35917863b88c
Jordan Ginor <jordan.ginor@gmail.com>
2019-01-02 14:16:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,7 +41,9 @@ def hit?(x)
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
+  get_user_input
 end
 
 #####################################################
